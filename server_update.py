7a89860d7d188c91d84f4fdc75246ccebb76982a
tilnow <44685516+tilnow@users.noreply.github.com>
2020-10-21 18:48:38 +0300

    Update server_update.py

--- server_update.py
+++ server_update.py
@@ -46,4 +46,4 @@ def is_valid_signature(x_hub_signature, data, private_key):
     return hmac.compare_digest(mac.hexdigest(), github_signature)
     
 if __name__ == "__main__":
-    app.run(ssl_context=('cert.pem', 'key.pem'))
+    app.run()#ssl_context=('cert.pem', 'key.pem'))
