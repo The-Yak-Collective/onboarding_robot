c0ed3fdfff86578021dcf6feea59ad0058385356
tilnow <44685516+tilnow@users.noreply.github.com>
2020-10-22 22:25:23 +0300

    Update csvofyaks.py

--- csvofyaks.py
+++ csvofyaks.py
@@ -49,7 +49,7 @@ async def on_message(message):
         await message.channel.send("pretend this is a csv file of all yaks")
         await message.channel.send("actual file?", file=discord.File("memberlist.csv"))
     if message.content.startswith('$test'):
-        await message.channel.send("this is a test")
+        await message.channel.send("this is a test: "+str([(x.name,x.created_at) for x in message.author.roles]))
     if (message.content.startswith('$die!') and message.author.id==710573356759384075):
         exit(0)
     if message.content.startswith('$dm'):
