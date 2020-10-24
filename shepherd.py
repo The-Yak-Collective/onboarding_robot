9747df590bef8b24b1646c0933088842607b9624
tilnow <44685516+tilnow@users.noreply.github.com>
2020-10-24 22:32:33 +0300

    Update shepherd.py

--- shepherd.py
+++ shepherd.py
@@ -11,23 +11,11 @@ from statemachine import * # this includes "machines", a list of machine dicts
 conn=0 #the connection should be global. curso, etc, also, but not so much
 db_c=0 #cursor
 
-class MyCog(commands.Cog):
-    def __init__(self):
-        self.index = 0
-        self.on_tick.start()
 
-    def cog_unload(self):
-        self.on_tick.cancel()
-
-    @tasks.loop(seconds=30.0) #change to 3600 as soon as we see this works
-    async def on_tick():
-        #check that db loaded... if not it means bot is not ready yet
-        print("check if any on_ticks need to be run, check first run_params for tick count to decide IF to run", index)
-
-@tasks.loop(seconds=13.0) #change to 3600 as soon as we see this works
+@tasks.loop(seconds=60.0) #change to 3600 as soon as we see this works. right now at 60, just so we see it happens multipel times
 async def test_tick():
     print("does this tick work?",time.time())
-test_tick.start()
+
 
 
 load_dotenv('.env')
@@ -44,7 +32,8 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client),  client.guilds)
     await update_database()
     setup_sm(machines)
-        
+    test_tick.start() #makes sure we have DB
+
 
 @client.event
 async def on_member_join(member):
@@ -56,7 +45,8 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
-    print("i would have checked this message:",message.content, message.channel)
+    print("i would have checked this message:",message.content, message.channel, message.author.id)
+    print(db_c.execute('select * from yakstates where discordid=(?)',(str(message.author.id),))
     #here add a test facility as well as other stuff
     #most important - add ignore me function in db, so we skip eveything if person asked to be ignored/frozen
     #also parse "$help"
@@ -66,23 +56,25 @@ async def update_database():
     mem=[]
     g=client.guilds[0]
     lastread=int(time.time())
-    mem=await g.fetch_members().flatten()
     conn=sqlite3.connect('statedatabase.db')
     db_c = conn.cursor()
     db_c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='yakstates' ''')
     if db_c.fetchone()[0]!=1:
         db_c.execute('''CREATE TABLE yakstates
-             (discordid text, machine text, state text, startedat int)''')
+             (discordid text, machine text, state text, startedat int, ignoreme int)''')
         db_c.execute('''CREATE TABLE lastread
              (timestamp int)''')
+        mem=await g.fetch_members().flatten()
         for m in machines:
             db_c.executemany('''insert into yakstates values
-             (?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread) for x in mem])# new yaks get benifit of doubt... as if they joined just now
+             (?, ?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread, 0) for x in mem])# new yaks get benifit of doubt... as if they joined just now
         db_c.execute('''UPDATE lastread
              set timestamp=(?)''',(lastread,))
     else:
         db_c.execute('''select * from lastread''')
         prevread=db_c.fetchone()[0]
+        mem=await g.fetch_members(after=datetime.datetime,fromtimestamp(prevread-10000)).flatten()
+        print("fetched only:",len(mem))
         print("prevread=",prevread)
         for m in machines:
             db_c.execute('''UPDATE lastread
@@ -90,7 +82,7 @@ async def update_database():
             mem1=[x for x in mem if datetime.datetime.timestamp(x.joined_at)>prevread]
             print("adding {} members to machine {}".format(len(mem1), m[1]))
             db_c.executemany('''insert into yakstates values
-             (?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread) for x in mem1])
+             (?, ?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread,0) for x in mem1])
     conn.commit()
 
 def update_db_new_member(member):
