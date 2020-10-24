09048041926bc8a839ac661aaefeb33984f0151a
tilnow <44685516+tilnow@users.noreply.github.com>
2020-10-24 22:59:43 +0300

    Update shepherd.py

--- shepherd.py
+++ shepherd.py
@@ -38,6 +38,7 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     update_db_new_member(member)
+    #and now also apply state machine on him? or wait for events? after all, he did just join a state
     
 
 
@@ -46,6 +47,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     print("i would have checked this message:",message.content, message.channel, message.author.id)
+    db_c = conn.cursor() # too much of this consider wrapping up in a function, also teh commit part, add user, etc
     print(db_c.execute('select * from yakstates where discordid=(?)',(str(message.author.id),)))
     #here add a test facility as well as other stuff
     #most important - add ignore me function in db, so we skip eveything if person asked to be ignored/frozen
@@ -68,25 +70,48 @@ async def update_database():
         for m in machines:
             db_c.executemany('''insert into yakstates values
              (?, ?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread, 0) for x in mem])# new yaks get benifit of doubt... as if they joined just now
+            for y in mem:
+                do_on_enter(m,y)
         db_c.execute('''UPDATE lastread
              set timestamp=(?)''',(lastread,))
     else:
         db_c.execute('''select * from lastread''')
         prevread=db_c.fetchone()[0]
-        mem=await g.fetch_members(after=datetime.datetime.fromtimestamp(prevread-(60*60*24*5))).flatten()
+        mem=await g.fetch_members(after=datetime.datetime.fromtimestamp(prevread)).flatten() # reads only ones added since prevread
         print("fetched only:",len(mem))
         print("prevread=",prevread)
         for m in machines:
             db_c.execute('''UPDATE lastread
              set timestamp=(?)''',(lastread,))
-            mem1=[x for x in mem if datetime.datetime.timestamp(x.joined_at)>prevread]
-            print("adding {} members to machine {}".format(len(mem1), m[1]))
+            #mem1=[x for x in mem if datetime.datetime.timestamp(x.joined_at)>prevread]
+            print("adding {} members to machine {}".format(len(mem), m[1]))
             db_c.executemany('''insert into yakstates values
-             (?, ?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread,0) for x in mem1])
+             (?, ?, ?, ?, ?)''',[(x.id,m[1],m[2],lastread,0) for x in mem])
+            for y in mem:
+                do_on_enter(m,y,m[2])
     conn.commit()
 
 def update_db_new_member(member):
-    print('add new member to db, if not already in it')
+    x=member
+    db_c=conn.cursor()
+    db_c.execute('select * from yakstates where discordid=(?)',(str(x.id),))
+    if not db_c.fetchone(): # is not in db yet
+        print(member.id, " not in db yet")
+        lastread=int(time.time()) #could not be exectly correct. so one solution is to run fetchall again. another is to check. which we did
+        for m in machines:
+            db_c.execute('''insert into yakstates values
+             (?, ?, ?, ?, ?)''',(x.id,m[1],m[2],lastread, 0))# new yaks get benifit of doubt... as if they joined just now
+        db_c.execute('''UPDATE lastread
+             set timestamp=(?)''',(lastread,))
+        conn.commit()
+        for m in machines:
+            do_on_enter(m,x,m[2])
+    print('add new member to db, if not already in it', member.name, member.id)
+
+def do_on_enter(m,x,state): # run when entering any new state, especially first one...
+    print("here we run the on_enter function")
+    print(m[1], state, m[0][state].onenter.__name__, m[0][state].onenter_params)
+
 
     #read database
 
