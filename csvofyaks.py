91a08f980a242d5343e8658a591431d3d9448022
tilnow <44685516+tilnow@users.noreply.github.com>
2020-11-11 12:26:11 +0200

    Update csvofyaks.py

--- csvofyaks.py
+++ csvofyaks.py
@@ -1,11 +1,26 @@
 import discord
 import base64
+
+
+import pickle
+import requests
+import os.path
 import os
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from dateutil.parser import parse
+
+from datetime import datetime, timedelta
+from pytz import timezone
+import pytz
+import time
 from dotenv import load_dotenv
-from icalevents.icalevents import events
+
 from datetime import datetime
 
-load_dotenv('~/.env')
+load_dotenv('/home/yak/.env')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -64,15 +79,39 @@ async def on_message(message):
         print("target is:",target,flush=True)    
         await target.send('Hello! i was told by '+message.author.name+' to contact you')
     if message.content.startswith('$upcoming'):
-        icalurl='https://calendar.google.com/calendar/ical/o995m43173bpslmhh49nmrp5i4%40group.calendar.google.com/public/basic.ics'
-        es=events(icalurl)
-        s="Upcoming in next week:\n"
-        for y in es:
-            tl=y.time_left()
-            days, hours, minutes = tl.days, tl.seconds // 3600, tl.seconds // 60 % 60
-            ts=str(tl.days) + ' days '
+        creds = None
+        if os.path.exists('/home/yak/token.pickle'):
+            with open('/home/yak/token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('yc-credentials.json', SCOPES)
+                creds = flow.run_local_server(port=9000)
+            with open('/home/yak/token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        cal = build('calendar', 'v3', credentials=creds)
+
+        now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        events_result = cal.events().list(calendarId='o995m43173bpslmhh49nmrp5i4@group.calendar.google.com', timeMin=now,timeMax=(datetime.utcnow()+timedelta(days=7)).isoformat()+ 'Z',
+                                            singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+        s="Upcoming in next week (beta version):\n"
+        if not events:
+            s=s+'No upcoming events found.'
+         
+
+        for event in events:
+            start = parse(event['start'].get('dateTime', event['start'].get('date')))
+            #print(start, datetime.utcnow(),datetime.now().astimezone())
+            seconds2go=(start-datetime.utcnow().astimezone()).total_seconds()
+            days, hours, minutes = seconds2go //(3600*24), seconds2go // 3600, seconds2go // 60 % 60
+
+            ts=str(days) + ' days '
             ts=ts+ ' and '+ str(hours)+ ' hours' +' and '+str(minutes)+ ' minutes'
-            s=s+y.summary+ ' starts in: '+ ts+'\n'
+            s=s+event.summary+ ' starts in: '+ ts+'\n'
         await message.channel.send(s)
 
 
