3b7d09637c2f312778c395fdbc984b760ac1d9ae
tilnow <44685516+tilnow@users.noreply.github.com>
2021-02-17 11:42:05 +0200

    Update csvofyaks.py

--- csvofyaks.py
+++ csvofyaks.py
@@ -320,8 +320,11 @@ async def on_message(message):
                 pass
             print(status.text)
             #post the tweet and sender in tweeter channel
-            ch=client.get_channel(TWITTER_CHAN)
-            await ch.send('<@{0}> sent a twit: {1}'.format(message.author.id, txt))
+            try:
+                ch=client.get_channel(TWITTER_CHAN)
+                await ch.send('<@{0}> sent a twit: {1}'.format(message.author.id, txt))
+            except:
+                pass
             await dm_chan.send('twit tweeted:'+txt)
             
         else:
