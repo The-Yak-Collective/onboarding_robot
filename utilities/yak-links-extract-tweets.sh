fa2a898c2f5f33de3ee760d5f2128d5b1edc1849
Nathan Acks <nathan.acks@cardboard-iguana.com>
2021-06-21 22:41:50 -0600

    Utility scripts for working with the $links command

---
+++ utilities/yak-links-extract-tweets.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+YAK_LINKS="$1"
+
+tail +2 "$1" \
+	| sed -e 's/^.*", "//' \
+	| sed -e 's/"$//' \
+	| sed -e 's/; /\n/g' \
+	| sed -e 's/\()\|),\|)\.\)$//' \
+	| sed -e 's/?s=.*$//' \
+	| cat -n \
+	| sort -uk2 \
+	| sort -n \
+	| cut -f2- \
+	| grep -E "https://(www\.)?twitter\.com/.*/status/.*"
