cfb3a9849d5bbcfb72b24f610cad0df0b9bfb8fd
Nathan Acks <nathan.acks@cardboard-iguana.com>
2021-09-04 12:42:43 -0600

    Fix

--- utilities/yak-tweets.sh
+++ utilities/yak-tweets.sh
@@ -8,9 +8,9 @@ tail +2 "$1" \
 	| sed -e 's/; /\n/g' \
 	| sed -e 's/\()\|),\|)\.\)$//' \
 	| sed -e 's/?s=.*$//' \
+	| sed -e 's/$/"/' \
 	| cat -n \
 	| sort -uk2 \
 	| sort -n \
 	| cut -f2- \
 	| grep -E "https://(www\.)?twitter\.com/.*/status/.*"
-	| sed -e 's/$/"/'
